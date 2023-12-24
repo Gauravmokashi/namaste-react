9c0498c796106ba2d245f4450880673b1140034d
Gaurav Mokashi <mokashigaurav1@gmail.com>
2023-12-24 12:52:06 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", { id: "child1" }, "I'm Child"),
+        React.createElement("div", { id: "child2" },
+            React.createElement("h1", { id: "Grand-Child" }, "I'm Grand Child"))
+    ]))
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading)
---
+++ README.md
@@ -0,0 +1,1 @@
+Namaste React ❤️🚀
